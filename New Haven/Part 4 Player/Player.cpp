d31fd94eb3e7dad682b4bfe603bf3d79ef1149fe
Luiz <luiz87_@hotmail.com>
2020-01-31 17:39:23 -0500

    Added some methods
    Started to create the methods. Also testing the new branch

--- New Haven/Part 4 Player/Player.cpp
+++ New Haven/Part 4 Player/Player.cpp
@@ -3,4 +3,33 @@
 
 using namespace std;
 
+void Player::PlaceHarvestTile()
+{
+	return;
+}
 
+void Player::DrawBuilding()
+{
+
+}
+
+void Player::DrawHarvestTile()
+{
+
+}
+
+void Player::ResourceTracker()
+{
+
+}
+
+void Player::BuildVillage()
+{
+
+}
+
+int Player::CalculateResources()
+{
+	//TODO return resources size. waiting for resources to be implemented
+	//return resources.size();
+}
--- New Haven/Part 4 Player/Player.h
+++ New Haven/Part 4 Player/Player.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <iostream>
+#include "Deck.h"
+
 class Player
 {
+public:
+	//Player(int playerNum);
+	//~Player();
+	void PlaceHarvestTile();
+	void DrawBuilding();
+	void DrawHarvestTile(); //TODO: possible parameters after having the resources implemented(std::shared_ptr<Deck> deck, int slotIndex, int price);
+	void ResourceTracker();
+	void BuildVillage();
+
+	int getPlayerNumber() 
+	{ 
+		return playerNumber; 
+	}
+
+	int CalculateResources();
+
+private:
+
+	int playerNumber;
+
 };
 
