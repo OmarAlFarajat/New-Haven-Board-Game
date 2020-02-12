5e8e02bd25e0220062b2c7e075b105698e6df675
unknown <triduc2311m@gmail.com>
2020-02-11 23:40:12 -0500

    Update resolve bugs after merge feature/part_5

--- New Haven/Assignment1/Resources.h
+++ New Haven/Assignment1/Resources.h
@@ -1,31 +1,80 @@
 #pragma once
-#include <string>
 
-using namespace std;
 /*
 TODO: implement draw() methods in HarvestDeck and BuildingDesk. Let's User use these methods
 to draw a tiles (building or harvest)
 */
 
+
+// Resource Class
+enum class ResourceType { TIMBER, STONE, WHEAT, SHEEP };
+class Resource
+{
+public:
+	Resource();
+	Resource(ResourceType type);
+	~Resource();
+
+private:
+	ResourceType* value;
+};
+
+// Harvest tiles Object
+class HarvestTiles
+{
+public:
+	HarvestTiles();
+	~HarvestTiles();
+	bool isContain(ResourceType type);
+
+private:
+	const static int numOfResource = 4;
+	Resource contain[numOfResource];
+
+};
+
+
 // The Deck of Harvest Tiles Object
-class HarvestDeck 
+class HarvestDeck
 {
 public:
 	HarvestDeck();
+	~HarvestDeck();
 	HarvestTiles draw();
 
 private:
 	const static int numOfTiles = 60; // 60 Harvest Tiles
 	static int numOfRemain;
-	HarvestTiles deck[numOfTiles];
+	static HarvestTiles deck[numOfTiles];
 
 };
 
+
+// Building tiles Object
+enum class BuildingType { MEADOW, QUARRY, FOREST, WHEATFIELD };
+enum class BuildingColor { GREEN, GREY, RED, YELLOW };
+class BuildingTiles
+{
+public:
+	BuildingTiles();
+	BuildingTiles(BuildingColor color, BuildingType buildingType, int value);
+	~BuildingTiles();
+	BuildingType getBuildingType();
+
+private:
+	BuildingType* Type;
+	BuildingColor* Color;
+	int* Value;    // 1 to 6
+
+};
+
+
 // The Deck of Building Object
-class BuildingDeck 
+class BuildingDeck
 {
 public:
 	BuildingDeck();
+	~BuildingDeck();
 	BuildingTiles draw();
 
 private:
@@ -35,53 +84,13 @@ private:
 
 };
 
-// Harvest tiles Object
-class HarvestTiles
-{
-public:
-	HarvestTiles();
-	bool isContain(ResourceType type);
-
-private:
-	const static int numOfResource = 4;
-	Resource contain[numOfResource];
-
-};
-
-enum class BuildingType {MEADOW, QUARRY, FOREST, WHEATFIELD};
-enum class BuildingColor {GREEN, GREY, RED, YELLOW};
-// Building tiles Object
-class BuildingTiles 
-{
-public:
-	BuildingTiles();
-	BuildingTiles(BuildingColor color, BuildingType buildingType, int value);
-	BuildingType getBuildingType();
-
-private:
-	BuildingType *Type; 
-	BuildingColor *Color; 
-	int *Value;    // 1 to 6
-
-};
-
-enum class ResourceType { TIMBER, STONE, WHEAT, SHEEP};
-class Resource
-{
-public:
-	Resource();
-	Resource(ResourceType type);
-
-private:
-	ResourceType *value;
-};
-
 
 // Hand Object 
 class hand
 {
 public:
 	hand();
+	~hand();
 	void exchange();
 
 private:
